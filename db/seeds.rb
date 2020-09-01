a65ee5f3cda57a7486b6d07593664b14a797b623
Alan Barton <abarton201@gmail.com>
2020-09-01 19:48:04 -0400

    Add product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.new(title: "Captain Morgan", price: 30, imiage_url: "captainmorgan.com", description: "spiced rum")
+products.save
