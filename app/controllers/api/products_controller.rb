115d8d9549342575c2184bf868858c0868393496
Alan Barton <abarton201@gmail.com>
2020-09-01 20:42:49 -0400

    all_products display on Server

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def all_product
+    @product = Product.all
+    render "all_product.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-products = Product.new(title: "Captain Morgan", price: 30, imiage_url: "captainmorgan.com", description: "spiced rum")
-products.save
+product = Product.new(name: "Captain Morgan", price: 30, image_url: "captainmorgan.com", description: "spiced rum")
+product.save
+product = Product.new(name: "Tacante", price: 3, image_url: "tacante.com", description: "beer")
+product.save
