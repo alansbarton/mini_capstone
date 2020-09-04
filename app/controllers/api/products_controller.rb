4ebaabd363697bcaccd0b6f563d32a0ee9a790b2
Alan Barton <abarton201@gmail.com>
2020-09-03 22:27:14 -0400

    add update and delete function

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -20,4 +20,22 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: id_number)
     render "show.json.jb"
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "item was deleted" }
+  end
 end
